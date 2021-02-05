649db66753469e79e5cc128206fb7b5ba3ecddb1
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-02-05 12:28:37 +0100

    Merged maths exceptions.

--- pom/maths/exceptions.hpp
+++ pom/maths/exceptions.hpp
@@ -1,3 +1,42 @@
 #pragma once
 
-#include "pom/maths/exception/precondition_violation.hpp"
+#include <string>
+
+namespace pom {
+namespace maths {
+
+class exception {
+public:
+	virtual const char* what() const noexcept = 0;
+};
+
+class invariant_violation : public exception {
+public:
+	invariant_violation(const std::string& what)
+		: what_{what}
+	{}
+
+	const char* what() const noexcept {
+		return what_.c_str();
+	}
+
+private:
+	std::string what_;
+};
+
+class precondition_violation : public exception {
+public:
+	precondition_violation(const std::string& what)
+		: what_{what}
+	{}
+
+	const char* what() const noexcept {
+		return what_.c_str();
+	}
+
+private:
+	std::string what_;
+};
+
+} // namespace maths
+} // namespace pom
