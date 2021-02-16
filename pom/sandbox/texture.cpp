7c6f01065224c0acf6dade1ed8cf1783d4521228
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-02-16 09:15:46 +0100

    Working minimalist example of outputing a png.

--- pom/sandbox/texture.cpp
+++ pom/sandbox/texture.cpp
@@ -1,5 +1,29 @@
+#include "pom/maths/matrix/all.hpp"
+#include "pom/maths_impl/all.hpp"
+
 #include <iostream>
+#include <QImage>
+
+using namespace pom;
+
+using namespace maths;
+using namespace maths_impl;
+
+template<matrix M>
+QImage to_image(const M& m) {
+	auto s = size(m);
+	auto im = QImage(at(s, 0), at(s, 1), QImage::Format_RGB32);
+	for(decltype(s) ij = {0, 0}; at(ij, 1) < at(s, 1); ++at(ij, 1)) {
+		for(at(ij, 0) = 0; at(ij, 0) < at(s, 0); ++at(ij, 0)) {
+			im.setPixelColor(at(ij, 0), at(ij, 1), qRgba(at(ij, 0), at(ij, 1), 0, 255));
+		}
+	}
+	return im;
+}
 
 int main() {
-	std::cout << "hi" << std::endl;
+	auto m = dynamic_matrix_<float>({255, 255});
+	static_assert(matrix<decltype(m)>);
+	auto i = to_image(m);
+	i.save("texture.png");
 }
