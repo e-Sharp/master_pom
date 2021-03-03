69bf4209afb52edd8b632eada76c49dee09dd277
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-03 11:21:28 +0100

    Playing with sand.

--- pom/sandbox/coloration.cpp
+++ pom/sandbox/coloration.cpp
@@ -2,9 +2,11 @@
 #include "pom/io_format/srtm/all.hpp"
 #include "pom/io_format/wavefront/all.hpp"
 #include "pom/maths/matrix/interpolation.hpp"
+#include "pom/maths/numeric.hpp"
 #include "pom/maths/noise.hpp"
 #include "pom/maths/vector/all.hpp"
 #include "pom/terrain/all.hpp"
+#include "pom/terrain/paths.hpp"
 
 #include <cmath>
 #include <iostream>
@@ -13,7 +15,7 @@
 #include <fstream>
 
 using namespace pom;
-using namespace maths;
+using namespace pom::maths;
 using namespace pom::maths_impl;
 using namespace pom::terrain;
 
@@ -51,60 +53,106 @@ static_vector<float, 3> color(static_vector<float, 3> xyz) {
 }
 
 void throwing_main() {
-    auto shf = heightfield{};
-    { // Reading SRTM.
-        shf.domain = maths_impl::vector<2>({
-            maths_impl::interval_0_n(1201.f * 90.f),
-            maths_impl::interval_0_n(1201.f * 90.f)});
-        auto srtm = io_std::open_file(
-            "D:/project/master_pom/data/srtmgl3_v003_hgt/N28E083.hgt",
-            std::ios::binary | std::ios::in);
-        shf.heights = io_format::srtm::read_srtm3(srtm);
-    }
-    { // Outputing mesh.
-        auto f = io_std::open_file("mesh.obj", std::ios::out);
-        io_format::wavefront::write(f, obj{shf});
-    }
-    { // Outputing gradients.
-        auto grads = maths_impl::same_size_matrix<decltype(maths_impl::vector<float, 2>())>(shf.heights);
-        for(auto ri : maths::row_indexes(grads))
-        for(auto ci : maths::col_indexes(grads)) {
-            auto g = at(grads, row(ri), col(ci)) = gradient(
-                [&](float x, float y) { return maths::bilerp(shf.heights, maths::col_i<float>(x), maths::row_i<float>(y)); },
-                maths_impl::vector<2>({float(ci), float(ri)}), 1.f);
-            // std::cout << at(g, 0) << " " << at(g, 1) << std::endl;
+    auto hf = heightfield{};
+    { // Reading data.
+        auto srtm = maths_impl::matrix<float>();
+        { // Reading SRTM.
+            auto srtm_file = io_std::open_file(
+                "d:/project/master_pom/data/srtmgl3_v003_hgt/n28e083.hgt",
+                std::ios::binary | std::ios::in);
+            srtm = io_format::srtm::read_srtm3(srtm_file);
+            for(auto&& e : row_major(srtm)) e /= 90.f;
         }
-        using color = decltype(maths_impl::vector<float, 3>());
-        auto gtex = same_size_matrix<color>(grads);
-        for(auto&& [d, s] : ranges::views::zip(row_major(gtex), row_major(grads))) {
-            auto g = s;
-            auto l = maths::length(g);
-            if(l > 1.f) {
-                g = 1.f / l * g;
+        hf.x_domain = hf.y_domain = maths_impl::interval_0_n(1201.f * 90.f / 90.f);
+        hf.heights = matrix_cr<float>(3000, 3000);
+        for(auto ri : maths::row_indexes(hf.heights)) 
+        for(auto ci : maths::col_indexes(hf.heights)) {
+            at_cr(hf.heights, ci, ri) = bilerp_normalized_cr(srtm,
+                float(ci) / (col_count(hf.heights) - 1),
+                float(ri) / (row_count(hf.heights) - 1));
+        }
+    }
+    if(false) { // Outputing mesh.
+        auto chunk_side = std::size_t{256};
+        auto cmax = positive_ceiled_quotient(col_count(hf.heights), chunk_side);
+        auto rmax = positive_ceiled_quotient(row_count(hf.heights), chunk_side);
+        
+        auto w = wavefront(view_cr(hf.heights, {0, 0}, {1, 1}));
+
+        auto ci_to_x = ci_to_x_mapping(hf);
+        auto ri_to_y = ri_to_y_mapping(hf);
+
+        for(std::size_t ri = 0; ri < rmax; ++ri) {
+            auto r1 = ri * chunk_side;
+            auto r2 = std::min(r1 + chunk_side, row_count(hf.heights) - 1);
+            for(std::size_t ci = 0; ci < cmax; ++ci) {
+                auto c1 = ci * chunk_side;
+                auto c2 = std::min(c1 + chunk_side, col_count(hf.heights) - 1);
+                if(c1 != c2 && r1 != r2){
+                    auto f = io_std::open_file(std::string(output_folder) + "/mesh_" + std::to_string(ci) + "_" + std::to_string(ri) + ".obj", std::ios::out);
+                    w.heights = view_cr(hf.heights, {c1, r1}, {c2 - c1, r2 - r1});
+                    w.x_domain = {ci_to_x(c1), ci_to_x(c2)};
+                    w.y_domain = {ri_to_y(r1), ri_to_y(r2)};
+                    io_format::wavefront::write(f, w);
+                }
             }
-            g = (g + 1.f) * 0.5f;
-            d = {at(g, 0), 0.f, at(g, 1)};
         }
-        to_image(gtex).save("gradients.png");
     }
-    { // Outputing texture.
-        auto tex = maths_impl::same_size_matrix<static_vector<float, 3>>(shf.heights);
-        auto ci_to_x = col_to_x_mapping(shf);
-        auto ri_to_y = row_to_y_mapping(shf);
-        for(auto ri : maths::row_indexes(shf.heights))
-        for(auto ci : maths::col_indexes(shf.heights)) {
-            auto x = ci_to_x(ci);
-            auto y = ri_to_y(ri);
-            auto z = at(shf.heights, maths::col_i(ci), maths::row_i(ri));
-            at(tex, maths::col_i(ci), maths::row_i(ri)) = color({x, y, z});
+    { // Outputing normals.
+        using normal = maths_impl::static_vector<float, 3>;
+        auto ns = maths_impl::same_size_matrix<normal>(hf.heights);
+        for(auto ri : maths::row_indexes(ns))
+        for(auto ci : maths::col_indexes(ns)) {
+            auto g = gradient(
+                [&](float x, float y) { return maths::bilerp_cr(hf.heights, x, y); },
+                maths_impl::vector<2>({float(ci), float(ri)}), 1.f);
+            auto gx = maths_impl::vector<3>({1.f, 0.f, at(g, 0)});
+            auto gy = maths_impl::vector<3>({0.f, 1.f, at(g, 1)});
+            auto n = cross(gx, gy);
+            n = n / length(n);
+            at_cr(ns, ci, ri) = n / 2.f + 0.5f;
         }
-        to_image(tex).save("texture.png");
+        to_image(ns).save((std::string(output_folder) + "/normals.png").c_str());
     }
+    //{ // Outputing gradients.
+    //    auto grads = maths_impl::same_size_matrix<decltype(maths_impl::vector<float, 2>())>(hf.heights);
+    //    for(auto ri : maths::row_indexes(grads))
+    //    for(auto ci : maths::col_indexes(grads)) {
+    //        auto g = at_cr(grads, ci, ri) = gradient(
+    //            [&](float x, float y) { return maths::bilerp_cr(hf.heights, x, y); },
+    //            maths_impl::vector<2>({float(ci), float(ri)}), 1.f);
+    //    }
+    //    using color = decltype(maths_impl::vector<float, 3>());
+    //    auto gtex = same_size_matrix<color>(grads);
+    //    for(auto&& [d, s] : maths::row_major(gtex, grads)) {
+    //        auto g = s / 10.f;
+    //        auto l = maths::length(g);
+    //        if(l > 1.f) {
+    //            g = 1.f / l * g;
+    //        }
+    //        g = (g + 1.f) / 2.f;
+    //        d = {at(g, 0), 0.f, at(g, 1)};
+    //    }
+    //    to_image(gtex).save("gradients.png");
+    //}
+    //{ // Outputing texture.
+    //    auto tex = maths_impl::same_size_matrix<static_vector<float, 3>>(hf.heights);
+    //    for(auto ri : maths::row_indexes(hf.heights))
+    //    for(auto ci : maths::col_indexes(hf.heights)) {
+    //        auto x = hf.c_to_x(ci);
+    //        auto y = hf.r_to_y(ri);
+    //        auto z = at_cr(hf.heights, ci, ri);
+    //        at_cr(tex, ci, ri) = color({x, y, z});
+    //    }
+    //    to_image(tex).save("texture.png");
+    //}
 }
 
 int main() {
     try {
         throwing_main();
+    } catch(const std::exception& e) {
+        std::cerr << "std::exception: " << e.what() << std::endl;
     } catch(...) {
         std::cerr << "Unhandled exception." << std::endl;
         return -1;
--- pom/sandbox/maths.cpp
+++ pom/sandbox/maths.cpp
@@ -1,20 +1,18 @@
-#include "pom/maths/matrix/interpolation.hpp"
+#include "pom/maths/vector/all.hpp"
 #include "pom/maths_impl/all.hpp"
 
+#include <iomanip>
 #include <iostream>
 #include <range/v3/all.hpp>
 #include <vector>
 
 using namespace pom;
 using namespace pom::maths_impl;
+using namespace ranges;
 
 int main() {
-	auto m = maths_impl::matrix<float>(2);
-	at(m, col(0), row(0)) = 0.f;
-	at(m, col(0), row(1)) = 1.f;
-	at(m, col(1), row(0)) = 1.f;
-	at(m, col(1), row(1)) = 2.f;
-	for(auto i = 0.f; i < 1.f; i += 0.1f) {
-		std::cout << bilerp(m, maths::col_i(i), maths::row_i(0.5f)) << std::endl;
+	auto v = std::vector<int>{-1, -2, -3, -4};
+	for(auto&& [f, s] : views::enumerate(v)) {
+		std::cout << s << std::endl;
 	}
 }
