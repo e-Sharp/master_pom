bd399f213a73c645ebe9b40bfcca0620ca18529b
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-03 23:13:00 +0100

    Playing with sand. Refactored example with new facilities.

--- pom/sandbox/coloration.cpp
+++ pom/sandbox/coloration.cpp
@@ -1,16 +1,18 @@
+#include "pom/io_qt/all.hpp"
 #include "pom/io_std/all.hpp"
 #include "pom/io_format/srtm/all.hpp"
 #include "pom/io_format/wavefront/all.hpp"
 #include "pom/maths/matrix/interpolation.hpp"
-#include "pom/maths/numeric.hpp"
-#include "pom/maths/noise.hpp"
 #include "pom/maths/vector/all.hpp"
+#include "pom/maths/gradient.hpp"
+#include "pom/maths/noise.hpp"
+#include "pom/maths/numeric.hpp"
 #include "pom/terrain/all.hpp"
+#include "pom/terrain/imports.hpp"
 #include "pom/terrain/paths.hpp"
 
 #include <cmath>
 #include <iostream>
-#include <QImage>
 #include <vector>
 #include <fstream>
 
@@ -18,34 +20,6 @@ using namespace pom;
 using namespace pom::maths;
 using namespace pom::maths_impl;
 using namespace pom::terrain;
-
-constexpr auto f_to_i(float f) {
-    return static_cast<int>(255.f * f + 0.5f);
-}
-
-template<maths::matrix M>
-QImage to_image(const M& m) {
-    auto im = QImage(static_cast<int>(col_count(m)), static_cast<int>(row_count(m)), QImage::Format_RGB32);
-    for(auto r : maths::row_indexes(m)) {
-        for(auto c : maths::col_indexes(m)) {
-            auto int_i = static_cast<int>(c);
-		    auto int_j = static_cast<int>(r);
-            auto col = at(row(m, r), c);
-            im.setPixelColor(int_i, int_j, qRgba(f_to_i(at(col, 0)), f_to_i(at(col, 1)), f_to_i(at(col, 2)), 255));
-        }
-    }
-    return im;
-}
-
-template<typename F2, maths::vector V>
-auto gradient(F2 f, V pos, float e) {
-    auto x = at(pos, 0);
-    auto y = at(pos, 1);
-	auto dx = (f(x + e, y    ) - f(x - e, y    )) / (2.f * e);
-	auto dy = (f(x    , y + e) - f(x    , y - e)) / (2.f * e);
-    auto g = maths_impl::vector<2>({dx, dy});
-    return maths_impl::vector<2>({dx, dy});
-}
 
 static_vector<float, 3> color(static_vector<float, 3> xyz) {
     //////////////////////////////////////////////////////////////////////////////////////////////// PAR ICI
@@ -72,7 +46,7 @@ void throwing_main() {
                 float(ri) / (row_count(hf.heights) - 1));
         }
     }
-    if(false) { // Outputing mesh.
+    { // Outputing mesh.
         auto chunk_side = std::size_t{256};
         auto cmax = positive_ceiled_quotient(col_count(hf.heights), chunk_side);
         auto rmax = positive_ceiled_quotient(row_count(hf.heights), chunk_side);
@@ -81,6 +55,8 @@ void throwing_main() {
 
         auto ci_to_x = ci_to_x_mapping(hf);
         auto ri_to_y = ri_to_y_mapping(hf);
+        auto ci_to_u = ci_to_u_mapping(hf);
+        auto ri_to_v = ri_to_v_mapping(hf);
 
         for(std::size_t ri = 0; ri < rmax; ++ri) {
             auto r1 = ri * chunk_side;
@@ -93,6 +69,8 @@ void throwing_main() {
                     w.heights = view_cr(hf.heights, {c1, r1}, {c2 - c1, r2 - r1});
                     w.x_domain = {ci_to_x(c1), ci_to_x(c2)};
                     w.y_domain = {ri_to_y(r1), ri_to_y(r2)};
+                    w.u_domain = {ci_to_u(c1), ci_to_u(c2)};
+                    w.v_domain = {ri_to_v(r1), ri_to_v(r2)};
                     io_format::wavefront::write(f, w);
                 }
             }
@@ -103,49 +81,26 @@ void throwing_main() {
         auto ns = maths_impl::same_size_matrix<normal>(hf.heights);
         for(auto ri : maths::row_indexes(ns))
         for(auto ci : maths::col_indexes(ns)) {
-            auto g = gradient(
-                [&](float x, float y) { return maths::bilerp_cr(hf.heights, x, y); },
-                maths_impl::vector<2>({float(ci), float(ri)}), 1.f);
-            auto gx = maths_impl::vector<3>({1.f, 0.f, at(g, 0)});
-            auto gy = maths_impl::vector<3>({0.f, 1.f, at(g, 1)});
-            auto n = cross(gx, gy);
-            n = n / length(n);
+            auto n = maths::normal_approximation(
+                [&](terrain::vec2f p) { return maths::bilerp_cr(hf.heights, at(p, 0), at(p, 1)); },
+                terrain::vec2f({float(ci), float(ri)}), 1.f);
             at_cr(ns, ci, ri) = n / 2.f + 0.5f;
         }
-        to_image(ns).save((std::string(output_folder) + "/normals.png").c_str());
+        io_qt::to_image(ns).save((std::string(output_folder) + "/normals.png").c_str());
     }
-    //{ // Outputing gradients.
-    //    auto grads = maths_impl::same_size_matrix<decltype(maths_impl::vector<float, 2>())>(hf.heights);
-    //    for(auto ri : maths::row_indexes(grads))
-    //    for(auto ci : maths::col_indexes(grads)) {
-    //        auto g = at_cr(grads, ci, ri) = gradient(
-    //            [&](float x, float y) { return maths::bilerp_cr(hf.heights, x, y); },
-    //            maths_impl::vector<2>({float(ci), float(ri)}), 1.f);
-    //    }
-    //    using color = decltype(maths_impl::vector<float, 3>());
-    //    auto gtex = same_size_matrix<color>(grads);
-    //    for(auto&& [d, s] : maths::row_major(gtex, grads)) {
-    //        auto g = s / 10.f;
-    //        auto l = maths::length(g);
-    //        if(l > 1.f) {
-    //            g = 1.f / l * g;
-    //        }
-    //        g = (g + 1.f) / 2.f;
-    //        d = {at(g, 0), 0.f, at(g, 1)};
-    //    }
-    //    to_image(gtex).save("gradients.png");
-    //}
-    //{ // Outputing texture.
-    //    auto tex = maths_impl::same_size_matrix<static_vector<float, 3>>(hf.heights);
-    //    for(auto ri : maths::row_indexes(hf.heights))
-    //    for(auto ci : maths::col_indexes(hf.heights)) {
-    //        auto x = hf.c_to_x(ci);
-    //        auto y = hf.r_to_y(ri);
-    //        auto z = at_cr(hf.heights, ci, ri);
-    //        at_cr(tex, ci, ri) = color({x, y, z});
-    //    }
-    //    to_image(tex).save("texture.png");
-    //}
+    { // Outputing texture.
+        auto tx = maths_impl::same_size_matrix<static_vector<float, 3>>(hf.heights);
+        auto ci_to_x = ci_to_x_mapping(hf);
+        auto ri_to_y = ri_to_y_mapping(hf);
+        for(auto ri : maths::row_indexes(hf.heights))
+        for(auto ci : maths::col_indexes(hf.heights)) {
+            auto x = ci_to_x(ci);
+            auto y = ri_to_y(ri);
+            auto z = at_cr(hf.heights, ci, ri);
+            at_cr(tx, ci, ri) = color({x, y, z});
+        }
+        io_qt::to_image(tx).save((std::string(output_folder) + "/texture.png").c_str());
+    }
 }
 
 int main() {
