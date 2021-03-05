a70eff2c8659161dbd2d16857fc2a5098a98154d
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-05 16:05:24 +0100

    Fixed namespace and real smoothstep.

--- pom/maths/interpolation.hpp
+++ pom/maths/interpolation.hpp
@@ -3,6 +3,7 @@
 #include <cmath>
 
 namespace pom {
+namespace maths {
 
 template<typename Ty, typename I> constexpr
 auto lerp(const Ty& lhs, const Ty& rhs, const I& t) {
@@ -20,10 +21,17 @@ auto smoothstep_1(const Ty& x) noexcept {
     return x * x * (-2 * x + 3);
 }
 
+template<typename Ty> constexpr
+auto smoothstep_1_(const Ty& x) noexcept {
+    if(x <= Ty(0))      return Ty(0);
+    else if(x >= Ty(1)) return Ty(1);
+    else                return x * x * (-2 * x + 3);
+}
+
 // First and second derivatives are null at 0 and 1.
 template<typename Ty> constexpr
 auto smoothstep_2(const Ty& x) noexcept {
     return x * x * x * (x * (6 * x - 15) + 10);
 }
 
-} // namespace pom
+}}
