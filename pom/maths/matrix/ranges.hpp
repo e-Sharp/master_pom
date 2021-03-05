d5d0b210d2d75c5020ad46b8c77b2d9014a8f314
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-05 16:04:18 +0100

    Matrix full index range.

--- pom/maths/matrix/ranges.hpp
+++ pom/maths/matrix/ranges.hpp
@@ -2,9 +2,10 @@
 
 #include "concepts.hpp"
 
-#include <range/v3/view/for_each.hpp>
+#include <range/v3/view/cartesian_product.hpp>
 #include <range/v3/view/iota.hpp>
-#include <range/v3/view/zip.hpp>
+#include <range/v3/view/transform.hpp>
+
 #include <tuple>
 
 namespace pom {
@@ -28,8 +29,20 @@ auto row_indexes(const M& m) {
 
 // TODO: col_major_indexes_cr.
 // TODO: col_major_indexes_rc.
-// TODO: row_major_indexes_cr.
-// TODO: row_major_indexes_rc.
+
+template<typename M> constexpr
+auto row_major_indexes_rc(const M& m) {
+	return ranges::views::cartesian_product(row_indexes(m), col_indexes(m));
+}
+
+template<typename M> constexpr
+auto row_major_indexes_cr(const M& m) {
+	return row_major_indexes_rc(m)
+	| ranges::views::transform([](auto rc) {
+		auto&& [r, c] = rc;
+		return std::make_tuple(c, r);
+	});
+}
 
 // Elements.
 
