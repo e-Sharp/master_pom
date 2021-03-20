613bd4344a0d237bae77b37f7200ea7224140117
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-20 15:51:50 +0100

    Added declarative sandbox.

--- pom/sandbox/declarative.cpp
+++ pom/sandbox/declarative.cpp
@@ -1,57 +1,34 @@
 #include <pom/declarative/all.hpp>
+
 #include <pom/meta/type/all.hpp>
 #include <pom/meta/type_list/all.hpp>
 
+#include <cmath>
 #include <iostream>
 #include <tuple>
 
 using namespace pom::decl;
 using namespace pom::meta;
 
+struct floor_ {};
 
-struct b {};
-struct c {};
-struct d {};
-struct e {};
-
-struct a {};
-
-consteval type_list<c, d> dependencies(type<a>) { return {}; }
-
-template<typename... D>
-a make(const data<D...> d, a) {
-	std::cout << "a" << std::endl;
-	return {};
+template<typename... D, typename Ty>
+Ty make(const data<D...>, floor_, Ty val) {
+	std::cout << "floor" << std::endl;
+	return std::floor(val);
 }
 
-consteval type_list<d> dependencies(type<b>) { return {}; }
+struct fract {};
 
-template<typename... D>
-int make(const data<D...>& d, b) {
-	std::cout << "b" << std::endl;
-	return {};
-}
+consteval type_list<floor_> dependencies(type<fract>) { return {}; }
 
-consteval type_list<e> dependencies(type<c>) { return {}; }
-
-template<typename... D>
-c make(const data<D...> d, c) {
-	std::cout << "c" << std::endl;
-	return {};
-}
-
-template<typename... D>
-d make(const data<D...> da, d) {
-	std::cout << "d" << std::endl;
-	return {};
-}
-
-template<typename... D>
-e make(const data<D...> d, e) {
-	std::cout << "e" << std::endl;
-	return {};
+template<typename... D, typename Ty>
+Ty make(const data<D...>& d, fract, Ty val) {
+	std::cout << "fract" << std::endl;
+	return val - d.get(type<floor_>());
 }
 
 int main() {
-	make_all<b>();
+	auto data = make_all<fract>(4.5);
+	std::cout << data.get(type<fract>()) << std::endl;
 }
