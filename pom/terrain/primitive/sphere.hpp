c475d254fb1061bc1be294370ee04b1f1af3be9c
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-22 14:04:39 +0100

    Rewrote sphere primitive with the declarative library.

--- pom/terrain/primitive/sphere.hpp
+++ pom/terrain/primitive/sphere.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "pom/terrain/imports.hpp"
+#include "pom/terrain/tag/all.hpp"
 
+#include <decl.hpp>
 #include <pom/maths/vector/all.hpp>
 
 #include <cmath>
@@ -11,25 +13,25 @@ namespace terrain {
 
 struct sphere {};
 
-float weight(sphere, vec2f pos) {
-	return std::sqrt(1 - maths::dot(pos, pos));
-}
-
-float height(sphere, vec2f pos) {
+template<typename C>
+float recipe(C, height_, sphere, vec2f pos) {
 	auto dd = maths::dot(pos, pos);
-	if(dd <= 1) {
-		return std::sqrt(1 - dd);
+	if(dd < 1) {
+		return std::sqrt(1.f - dd);
 	} else {
-		return 0;
+		return 0.f;
 	}
 }
 
-vec3f normal(sphere, vec2f pos) {
-	auto dd = maths::dot(pos, pos);
-	if(dd <= 1) {
-		return vec3f({at(pos, 0), at(pos, 1), std::sqrt(1 - dd)});
-	} else {
+auto prerequisites(normal_, sphere, vec2f) -> decl::targets<height_>;
+
+template<typename C>
+vec3f recipe(C c, normal_, sphere, vec2f pos) {
+	auto h = c.ref<height_>();
+	if(h == 0.f) {
 		return vec3f({0.f, 0.f, 1.f});
+	} else {
+		return vec3f({at(pos, 0), at(pos, 1), h});
 	}
 }
 
