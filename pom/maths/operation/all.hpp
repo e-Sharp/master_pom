2a4fc71322693a28742aae31ef72d3527227961c
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-26 19:11:05 +0100

    Refactored interpolation operations.

--- pom/maths/operation/all.hpp
+++ pom/maths/operation/all.hpp
@@ -2,7 +2,9 @@
 
 #include "noise/all.hpp"
 #include "clamp.hpp"
+#include "cubic_step.hpp"
 #include "floor.hpp"
 #include "fract.hpp"
+#include "lerp.hpp"
 #include "interpolation.hpp"
 #include "smoothstep.hpp"
