4c04cad9b269f13a1d899cc2fac8c87026d4b0f5
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-27 19:07:27 +0100

    decl::at now returns an integral constant.

--- lib/declarative/src/decl/type_list/at.hpp
+++ lib/declarative/src/decl/type_list/at.hpp
@@ -8,14 +8,14 @@
 
 namespace decl {
 
-template<std::size_t N, typename... TL> consteval
-auto at(type_list<TL...> l) {
+template<std::size_t I, typename... TL> consteval
+auto at(type_list<TL...> l, std::integral_constant<std::size_t, I> = {}) {
 	if constexpr(empty(l)) {
 		static_assert(false, "decl: Out of range.");
-	} else if constexpr(N == 0) {
+	} else if constexpr(I == 0) {
 		return head(l);
 	} else {
-		return at<N - 1>(tail(l));
+		return at(tail(l), std::integral_constant<std::size_t, I - 1>());
 	}
 }
 
