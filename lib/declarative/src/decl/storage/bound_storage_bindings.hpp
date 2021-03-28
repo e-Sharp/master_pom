cd83baf2adf3fce136e1060cdca82d46e99ca88a
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-28 08:14:37 +0200

    Fixed tuple_element which was wrong.

--- lib/declarative/src/decl/storage/bound_storage_bindings.hpp
+++ lib/declarative/src/decl/storage/bound_storage_bindings.hpp
@@ -12,7 +12,7 @@ namespace std {
 
 template<std::size_t I, typename S>
 struct tuple_element<I, decl::detail::bound_storage<S>> {
-	using type = decltype(decl::detail::what(at<I>(S::storage_traits::recipes())));
+	using type = decltype(decl::detail::what(at(S::storage_traits::recipes(), index(S::storage_traits::targets(), at<I>(S::bound_targets())))));
 };
 
 template<typename S>
