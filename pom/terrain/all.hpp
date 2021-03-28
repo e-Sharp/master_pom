7913fe0ab86cc5428da82eca22e2ad859abd2028
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-28 17:59:02 +0200

    Included operators.

--- pom/terrain/all.hpp
+++ pom/terrain/all.hpp
@@ -3,13 +3,11 @@
 #include "paths.hpp"
 #include "wavefront.hpp"
 
-#include "pom/maths/all.hpp"
 #include "pom/terrain/heightfield.hpp"
 #include "pom/terrain/tesselation.hpp"
 
-
-
 #include "defaults.hpp"
 
+#include "landform/all.hpp"
+#include "operator/all.hpp"
 #include "primitive/all.hpp"
-#include "warping/noise_warping.hpp"
