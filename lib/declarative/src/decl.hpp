d47e1393c7e323af9db2638e2d3d886415e3b17b
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-28 08:15:12 +0200

    Added missing include.

--- lib/declarative/src/decl.hpp
+++ lib/declarative/src/decl.hpp
@@ -2,6 +2,7 @@
 
 #include "decl/make/make.hpp"
 #include "decl/storage/bound_storage_bindings.hpp"
+#include "decl/storage/bound_storage_get.hpp"
 
 #include "decl/type/type_of.hpp"
 #include "decl/type/what.hpp"
