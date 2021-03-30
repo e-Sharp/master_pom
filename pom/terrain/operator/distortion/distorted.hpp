7e67f388a35c82fd7220e0103023714a0ae7b301
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-30 17:17:06 +0200

    Added distortion operator.

--- pom/terrain/operator/distortion/distorted.hpp
+++ pom/terrain/operator/distortion/distorted.hpp
@@ -1,13 +1,10 @@
 #pragma once
 
+#include "distortion.hpp"
+
 #include "pom/terrain/import/all.hpp"
 
 namespace pom::terrain {
-
-struct distortion {
-	float amplitude = 1.f;
-	float frequency = 1.f;
-};
 
 template<typename Terrain>
 struct distorted {
@@ -18,9 +15,9 @@ struct distorted {
 template<typename T>
 distorted(distortion, T&&) -> distorted<T>;
 
-template<typename T> constexpr
-auto operator|(T&& t, distortion d) {
-	return distorted(std::move(d), std::move(t));
+template<typename Terrain> constexpr
+auto operator|(Terrain&& t, distortion d) {
+	return distorted(std::move(d), std::forward<Terrain>(t));
 }
 
 template<typename DeclContext, typename T> constexpr
