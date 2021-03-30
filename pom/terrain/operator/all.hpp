31d13b8ed25f9a14b1539bc4f46129b5a5e74c7e
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-30 17:40:58 +0200

    Updated include.

--- pom/terrain/operator/all.hpp
+++ pom/terrain/operator/all.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "distortion/distorted.hpp"
+#include "scaling/scaled.hpp"
 #include "sum/sum.hpp"
 #include "sum/sum_operation.hpp"
 #include "translation/translated.hpp"
 #include "amplification.hpp"
-#include "scaling.hpp"
