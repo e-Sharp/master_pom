ec14744ebd841101e604e21efa7a18aa4d96fe9f
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-30 17:17:54 +0200

    Added translation operator.

---
+++ pom/terrain/operator/translation/translated.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "translation.hpp"
+
+#include "pom/terrain/import/all.hpp"
+
+namespace pom::terrain {
+
+template<typename Terrain>
+struct translated {
+	translation translation = {};
+	Terrain terrain;
+};
+
+template<typename T>
+translated(translation, T&&) -> translated<T>;
+
+template<typename Terrain> constexpr
+auto operator|(Terrain&& te, translation tr) {
+	return translated(std::move(tr), std::forward<Terrain>(te));
+}
+
+template<typename DeclContext, typename T> constexpr
+vec3f recipe(DeclContext, gradient, const translated<T>& t, vec2f coords) {
+	auto& tr = t.translation.value;
+	auto tcs = coords + xy(tr);
+	auto [g] = decl::make<gradient>(t.terrain, tcs);
+	return g;
+}
+
+template<typename DeclContext, typename T> constexpr
+float recipe(DeclContext, height_, const translated<T>& t, vec2f coords) {
+	auto& tr = t.translation.value;
+	auto tcs = coords - xy(tr);
+	auto [h] = decl::make<height_>(t.terrain, tcs);
+	return h + z(tr);
+}
+
+}
---
+++ pom/terrain/operator/translation/translation.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "pom/terrain/import/all.hpp"
+
+namespace pom::terrain {
+
+struct translation {
+	vec3f value = {};
+};
+
+}
