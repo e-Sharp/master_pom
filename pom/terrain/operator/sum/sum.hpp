02b9ed1e4d9ee6ee81222a662b8fc05dddc58ae2
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-30 17:17:41 +0200

    Added sum operator.

---
+++ pom/terrain/operator/sum/sum.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "sum_operation.hpp"
+
+#include <tuple>
+
+namespace pom::terrain {
+
+struct sum {};
+
+template<typename Terrain, typename... Terrains> constexpr
+auto sum_of(Terrain&& t, Terrains&&... ts) {
+	return sum_operation(
+		std::forward<Terrain>(t),
+		sum_of(std::forward<Terrains>(ts)...));
+}
+
+template<typename Terrain> constexpr
+auto sum_of(Terrain&& t) {
+	return std::forward<Terrain>(t);
+}
+
+template<typename... T> constexpr
+auto operator|(std::tuple<T...>&& t, sum) {
+	return std::apply(
+		[](auto&&... args) { 
+			return sum_of(std::forward<decltype(args)>(args)...); },
+		std::move(t));
+}
+
+}
