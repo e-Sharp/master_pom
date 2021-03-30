4397dfefb69ef07e47e80ef09bc8f5deec33d9ef
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-30 23:41:51 +0200

    Updated operator includes.

--- pom/terrain/operator/all.hpp
+++ pom/terrain/operator/all.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
+#include "amplification/amplified.hpp"
 #include "distortion/distorted.hpp"
+#include "offset/offset.hpp"
+#include "offset/offsetted.hpp"
 #include "scaling/scaled.hpp"
+#include "scaling/scaling.hpp"
 #include "sum/sum.hpp"
 #include "sum/sum_operation.hpp"
 #include "translation/translated.hpp"
-#include "amplification.hpp"
