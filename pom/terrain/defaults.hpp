a5746f2944833f9de6348719e9e5b9a420dbe67d
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-03-30 17:36:15 +0200

    Terrain queries now default to throwin a not implemented exception.

--- pom/terrain/defaults.hpp
+++ pom/terrain/defaults.hpp
@@ -7,22 +7,22 @@ namespace terrain {
 
 template<typename DeclContext, typename Terrain>
 vec3f recipe(DeclContext, color_, const Terrain&, vec2f) {
-	return vec3f({0.5f, 0.5f, 0.5f});
+	throw meta::not_implemented();
 }
 
 template<typename DeclContext, typename Terrain>
 float recipe(DeclContext, height_, const Terrain&, vec2f) {
-	return 0.f;
+	throw meta::not_implemented();
 }
 
 template<typename DeclContext, typename Terrain>
 vec3f recipe(DeclContext, normal_, const Terrain&, vec2f) {
-	return vec3f({0.f, 0.f, 1.f});
+	throw meta::not_implemented();
 }
 
 template<typename DeclContext, typename Terrain>
 float recipe(DeclContext, weight_, const Terrain&, vec2f) {
-	return 1.f;
+	throw meta::not_implemented();
 }
 
 }}
