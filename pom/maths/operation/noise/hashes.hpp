5e37fbb4a60c9e7c851f0beb6dfd4dfee6d1b9f6
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-04 11:35:10 +0200

    Fixed gradient noise hash overflow.

--- pom/maths/operation/noise/hashes.hpp
+++ pom/maths/operation/noise/hashes.hpp
@@ -2,18 +2,21 @@
 
 #include "pom/maths/vector/all.hpp"
 
+#include <cmath>
+
 namespace pom::maths {
 
 constexpr static_vector<float, 2> hash_1(static_vector<float, 2> x) {
-	auto k = vector_of(0.3183099f, 0.3678794f);
-    x = x * k + flipped(x);
-    return -1.f + 2.f * fract(16.f * k * fract(at(x, 0) * at(x, 1) * (at(x, 0) + at(x, 1))));
+	return vector_of(
+		fract(std::sin(dot(x, vector_of(27.609f, 57.583f))) * 43758.5453f),
+		fract(std::sin(dot(x, vector_of(33.211f, 53.899f))) * 74511.6299f)
+	) * 2.f - 1.f;
 }
 
 constexpr static_vector<float, 2> hash_2(static_vector<float, 2> x) {
-	auto k = vector_of(0.7283409f, 0.1720874f);
-    x = x * k + flipped(x);
-    return -1.f + 2.f * fract(16.f * k * fract(at(x, 0) * at(x, 1) * (at(x, 0) + at(x, 1))));
+	return vector_of(
+		std::sin(dot(x, vector_of(37.699f, 27.953f))) * 23088.9299f,
+		std::sin(dot(x, vector_of(63.541f, 13.063f))) * 44285.6371f);
 }
 
 }
