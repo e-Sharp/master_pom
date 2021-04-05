bf425c685e3e4e2255e49a2f78556511675f6606
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-05 20:16:53 +0200

    Updated lerp operation.

--- pom/terrain/operator/lerp/lerped.hpp
+++ pom/terrain/operator/lerp/lerped.hpp
@@ -2,7 +2,7 @@
 
 #include "lerp.hpp"
 
-#include "pom/terrain/operator/amplification/amplified.hpp"
+#include "pom/terrain/operator/scaling/scaling.hpp"
 #include "pom/terrain/operator/sum/sum.hpp"
 #include "pom/terrain/operator/sum/sum_operation.hpp"
 
@@ -13,8 +13,8 @@ namespace pom::terrain {
 template<typename Terrain0, typename Terrain1> constexpr
 auto operator|(std::tuple<Terrain0, Terrain1>&& t, lerp l) {
 	return std::forward_as_tuple(
-		get<0>(std::move(t)) | amplification(l.t),
-		get<1>(std::move(t)) | amplification(1.f - l.t))
+		get<0>(std::move(t)) | scaling(1.f, 1.f, l.t),
+		get<1>(std::move(t)) | scaling(1.f, 1.f, 1.f - l.t))
 	| sum();
 }
 
