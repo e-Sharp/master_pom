40e563982a7356cc07f334ef957936e832a8b6f8
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-05 16:00:49 +0200

    Commented out default recipes.

--- pom/terrain/defaults.hpp
+++ pom/terrain/defaults.hpp
@@ -1,28 +1,28 @@
-#pragma once
-
-#include "pom/terrain/import/all.hpp"
-
-namespace pom {
-namespace terrain {
-
-template<typename DeclContext, typename Terrain>
-vec3f recipe(DeclContext, color_, const Terrain&, vec2f) {
-	throw meta::not_implemented();
-}
-
-template<typename DeclContext, typename Terrain>
-float recipe(DeclContext, height_, const Terrain&, vec2f) {
-	throw meta::not_implemented();
-}
-
-template<typename DeclContext, typename Terrain>
-vec3f recipe(DeclContext, normal_, const Terrain&, vec2f) {
-	throw meta::not_implemented();
-}
-
-template<typename DeclContext, typename Terrain>
-float recipe(DeclContext, weight_, const Terrain&, vec2f) {
-	throw meta::not_implemented();
-}
-
-}}
+//#pragma once
+//
+//#include "pom/terrain/import/all.hpp"
+//
+//namespace pom {
+//namespace terrain {
+//
+//template<typename DeclContext, typename Terrain>
+//vec3f recipe(DeclContext, color_, const Terrain&, vec2f) {
+//	throw meta::not_implemented();
+//}
+//
+//template<typename DeclContext, typename Terrain>
+//float recipe(DeclContext, height_, const Terrain&, vec2f) {
+//	throw meta::not_implemented();
+//}
+//
+//template<typename DeclContext, typename Terrain>
+//vec3f recipe(DeclContext, normal_, const Terrain&, vec2f) {
+//	throw meta::not_implemented();
+//}
+//
+//template<typename DeclContext, typename Terrain>
+//float recipe(DeclContext, weight_, const Terrain&, vec2f) {
+//	throw meta::not_implemented();
+//}
+//
+//}}
