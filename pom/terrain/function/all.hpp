f6c9d118d807b22a8b1cf9c8dbc8cea4cebffbe3
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-06 16:36:00 +0200

    Grouped function includes.

---
+++ pom/terrain/function/all.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "addition/all.hpp"
+#include "constant/all.hpp"
+#include "lerp/all.hpp"
+#include "multiplication/all.hpp"
+#include "noise/all.hpp"
+#include "product/all.hpp"
+#include "sin/all.hpp"
+#include "sum/all.hpp"
+#include "tag/all.hpp"
