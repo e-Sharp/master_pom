27da3677520c1b21dff2a2b2d2eca4aeb7dd408d
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-06 16:34:51 +0200

    Added product.

---
+++ pom/terrain/function/product/product.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "pom/terrain/function/concept/function.hpp"
+
+namespace pom::terrain {
+
+template<function Lhs, function Rhs>
+struct product {
+	template<typename Lhs, typename... Rhss>
+	constexpr product(Lhs&& l, Rhss&&... rs)
+		: lhs(std::forward<Lhs>(l))
+		, rhs(product(std::forward<Rhss>(rs)...))
+	{}
+
+	template<typename Lhs, typename Rhs>
+	constexpr product(Lhs&& l, Rhs&& r)
+		: lhs(std::forward<Lhs>(l))
+		, rhs(std::forward<Rhs>(r))
+	{}
+
+	Lhs lhs;
+	Rhs rhs;
+};
+
+template<typename Lhs, typename... Rhss>
+product(Lhs, Rhss...) -> product<Lhs, decltype(product(Rhss...))>;
+
+template<typename Lhs, typename Rhs>
+product(Lhs, Rhs) -> product<Lhs, Rhs>;
+
+template<typename... S>
+struct is_first_order_function<product<S...>>
+: std::true_type {};
+
+}
