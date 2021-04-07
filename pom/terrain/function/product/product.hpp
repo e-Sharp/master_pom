c35d55a66e08ee6fa7fd4dcc8c2ce8b09f1e72cc
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-07 11:42:49 +0200

    Fixed product constructor.

--- pom/terrain/function/product/product.hpp
+++ pom/terrain/function/product/product.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
+#include "pom/terrain/function/concept/first_order_function.hpp"
 #include "pom/terrain/function/concept/function.hpp"
 
 namespace pom::terrain {
 
 template<function Lhs, function Rhs>
 struct product {
-	template<typename Lhs, typename... Rhss>
+	template<typename... Rhss>
 	constexpr product(Lhs&& l, Rhss&&... rs)
 		: lhs(std::forward<Lhs>(l))
 		, rhs(product(std::forward<Rhss>(rs)...))
 	{}
 
-	template<typename Lhs, typename Rhs>
 	constexpr product(Lhs&& l, Rhs&& r)
 		: lhs(std::forward<Lhs>(l))
 		, rhs(std::forward<Rhs>(r))
