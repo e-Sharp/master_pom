30b2964fb7f666e1e04dc6739f960e4982a428df
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-07 11:40:15 +0200

    Added shortcut for multiplication specialization.

--- pom/terrain/function/multiplication/multiplication_operator.hpp
+++ pom/terrain/function/multiplication/multiplication_operator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "pom/terrain/function/concept/function.hpp"
+#include "pom/terrain/function/constant/all.hpp"
 #include "pom/terrain/function/product/product.hpp"
 
 namespace pom::terrain {
@@ -9,5 +10,16 @@ template<function Lhs, function Rhs> constexpr
 auto operator*(Lhs&& l, Rhs&& r) {
 	return product(std::forward<Lhs>(l), std::forward<Rhs>(r));
 }
+
+template<function Lhs> constexpr
+auto operator*(Lhs&& l, float r) {
+	return product(std::forward<Lhs>(l), constant(r));
+}
+
+template<function Rhs> constexpr
+auto operator*(float l, Rhs&& r) {
+	return product(constant(l), std::forward<Rhs>(r));
+}
+
 
 }
