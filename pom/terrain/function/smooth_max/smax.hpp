615853ecdf07d182c39df7b097ad015cf3f1539d
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-07 22:42:51 +0200

    Rewrote max function in terms of opposite function.

--- pom/terrain/function/smooth_max/smax.hpp
+++ pom/terrain/function/smooth_max/smax.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
+#include "pom/terrain/function/opposite/all.hpp"
 #include "pom/terrain/function/smooth_min/all.hpp"
 
 namespace pom::terrain {
 
 template<typename A, typename B> constexpr
 auto smax(A&& a, B&& b, float smooth) {
-	return constant(-1.f) * smin(
-		constant(-1.f) * std::forward<A>(a),
-		constant(-1.f) * std::forward<B>(b),
+	return - smin(
+		- std::forward<A>(a),
+		- std::forward<B>(b),
 		smooth);
 }
 
