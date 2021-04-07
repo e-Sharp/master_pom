7047ead557e2e2141aa3a98bb56c4f327f910d05
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-07 22:41:55 +0200

    Rewrote lerp function.

--- pom/terrain/function/lerp/lerp.hpp
+++ pom/terrain/function/lerp/lerp.hpp
@@ -1,14 +1,12 @@
 #pragma once
 
-#include "pom/terrain/function/addition/all.hpp"
-#include "pom/terrain/function/concept/function.hpp"
-#include "pom/terrain/function/multiplication/multiplication_operator.hpp"
+#include "pom/terrain/function/lerped/all.hpp"
 
 namespace pom::terrain {
 
-template<function A, typename B, typename I> constexpr
+template<typename A, typename B, typename I> constexpr
 auto lerp(A a, B b, I i) {
-	return i * a + (1.f + -1.f * i) * b;
+	return lerped(std::move(a), std::move(b), std::move(i));
 }
 
 }
