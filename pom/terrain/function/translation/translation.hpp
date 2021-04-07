24c7d91e347489798f2676b94ae29258506eb105
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-07 11:42:29 +0200

    Added translation (value).

--- pom/terrain/function/translation/translation.hpp
+++ pom/terrain/function/translation/translation.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include "pom/terrain/function/concept/first_order_function.hpp"
+#include "pom/terrain/function/concept/higher_order_function.hpp"
 #include "pom/terrain/import/all.hpp"
 
 namespace pom::terrain {
 
 struct translation {
 	translation() = delete;
+
+	constexpr translation(float x, float y) noexcept
+		: value(std::move(x), std::move(y))
+	{}
 
 	constexpr translation(vec2f xy) noexcept
 		: value(std::move(xy))
@@ -17,5 +23,9 @@ struct translation {
 
 	vec2f value;
 };
+
+template<>
+struct is_higher_order_function<translation>
+: std::true_type {};
 
 }
