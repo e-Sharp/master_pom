c52ac6a62f9968168c3e00fec2d1ff793cf10b96
Willy <willy.jacquet@etu.univ-lyon1.fr>
2021-04-07 11:39:52 +0200

    Added + specialization for floats.

--- pom/terrain/function/addition/addition_operator.hpp
+++ pom/terrain/function/addition/addition_operator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "pom/terrain/function/concept/function.hpp"
+#include "pom/terrain/function/constant/all.hpp"
 #include "pom/terrain/function/sum/sum.hpp"
 
 namespace pom::terrain {
@@ -8,6 +9,16 @@ namespace pom::terrain {
 template<function Lhs, function Rhs> constexpr
 auto operator+(Lhs&& l, Rhs&& r) {
 	return sum(std::forward<Lhs>(l), std::forward<Rhs>(r));
+}
+
+template<function Lhs, function Rhs> constexpr
+auto operator+(Lhs&& l, float r) {
+	return sum(std::forward<Lhs>(l), constant(r));
+}
+
+template<function Lhs, function Rhs> constexpr
+auto operator+(float l, Rhs&& r) {
+	return sum(constant(l), std::forward<Rhs>(r));
 }
 
 }
